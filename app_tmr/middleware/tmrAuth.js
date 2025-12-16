0f8df39dc7741e186c166e3be59aefa91c415db5
Thiago_DESKTOP <th.space.desenvolvimento@gmail.com>
2025-12-16 10:53:48 -0300

    ve ai

--- app_tmr/middleware/tmrAuth.js
+++ app_tmr/middleware/tmrAuth.js
@@ -2,10 +2,11 @@
 // Semelhante ao middleware do app_he
 
 function tmrAuth(req, res, next) {
-    if (!req.session.user) {
-        return res.redirect('/login?redirect=/tmr');
-    }
-    next();
+  if (!req.session.user) {
+    const redirect = encodeURIComponent(req.originalUrl);
+    return res.redirect(`/login?redirect=${redirect}`);
+  }
+  next();
 }
 
 module.exports = tmrAuth;
